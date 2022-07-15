d04192c9d402b39f06c824eade490bfab32d0792
Sebastian <sebastianreyes2105@gmail.com>
2022-07-15 16:50:29 -0500

    agregando modelos, y componentes necesarios para el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/client/home/home.component';
+import { NavbarComponent } from './pages/client/navbar/navbar.component';
+import { BarraDemografiasComponent } from './pages/client/barra-demografias/barra-demografias.component';
+import { FooterComponent } from './pages/client/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    BarraDemografiasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/client/barra-demografias/barra-demografias.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BarraDemografiasComponent } from './barra-demografias.component';
+
+describe('BarraDemografiasComponent', () => {
+  let component: BarraDemografiasComponent;
+  let fixture: ComponentFixture<BarraDemografiasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BarraDemografiasComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BarraDemografiasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
